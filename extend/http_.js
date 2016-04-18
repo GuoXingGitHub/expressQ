1c4507b3519535e31177fa5213eff7721dc875f5
dingyong666 <1121744186@qq.com>
2016-04-18 17:46:37 +0800

    http_.js

---
+++ extend/http_.js
@@ -0,0 +1,74 @@
+/**
+ * Created by 桥下红药ҩ on 2016/1/30 0030.
+ * http 请求模块
+ *
+ * 示例
+ * req.url('http://fyyh.net').cookie('key=123').get( function (body) {
+    console.log(body)
+});
+ */
+var iconv = require("iconv-lite");
+iconv.extendNodeEncodings();
+var cheerio = require('cheerio');
+var fs = require('fs');
+var http = require('request').defaults({jar: true});
+function http_(){
+    var that = this;
+    var j = http.jar();
+    var encode = 'utf-8';
+    var url = '';
+    var mcookie = '';
+    var method = 'GET';
+    this.url = function (a) {
+        url = a;
+        return that;
+    };
+    this.cookie = function (b) {
+        mcookie = b;
+        return that;
+    };
+    this.post = function (callback) {
+        method = 'POST';
+        send(callback);
+    };
+    this.get = function (callback) {
+        method = 'GET';
+        send(callback);
+    };
+    this.encoding = function (e) {
+        encode = encoding;
+        return that;
+    };
+
+    var send = function (callback) {
+        if(mcookie!=''){
+            var cookie = http.cookie(mcookie);
+            j.setCookie(cookie, url);
+        }
+        var opt = {
+            method:method,
+            url: url,
+            encoding: encode,
+            headers: {
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+                'Referer': url,
+                'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+                'content-encoding': encode,
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0'
+            }
+        };
+
+        http(opt, function (error, response, body) {
+            if (response.statusCode == 200) {
+                return callback(body);
+            }
+            return null;
+        });
+    }
+
+}
+
+module.exports = http_;
+
+
